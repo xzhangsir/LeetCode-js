626f0b31db4dd625f71838831af931b11c425f43
张鑫 <2916733574@qq.com>
2022-06-03 22:59:19 +0800

    26 splice

---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=javascript
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length - 1; ) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1)
+    } else {
+      i++
+    }
+  }
+  return nums.length
+}
+// @lc code=end
