c5008c27d814925be990cc131353bd4af6d864ef
zhangxin <2916733574@qq.com>
2022-06-30 23:26:18 +0800

    31day 二分法

---
+++ 69.x-的平方根.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=javascript
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+//二分法
+var mySqrt = function (x) {
+  let left = 0,
+    right = x,
+    res = null
+  while (left <= right) {
+    let center = parseInt((right + left) / 2)
+    if (center * center <= x) {
+      res = center
+      left = center + 1
+    } else if (center * center > x) {
+      right = center - 1
+    }
+    console.log(left, right, center)
+  }
+  return res
+}
+// @lc code=end
