7ac33c08f05cc7a87ae4a08d8bf6d8049e0f938a
张鑫 <2916733574@qq.com>
2022-08-06 23:04:16 +0800

    暴力

---
+++ 数组/349.两个数组的交集.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  let res = []
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j] && !res.includes(nums1[i])) {
+        res.push(nums1[i])
+        break
+      }
+    }
+  }
+  return res
+}
+// @lc code=end
